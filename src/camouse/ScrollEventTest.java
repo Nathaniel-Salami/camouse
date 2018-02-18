861670f30f68504f9e4702313e7342a60a73c145
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-02-18 02:05:37 -0500

    Changed the scroll event test file, specifically the way the mouseMovement function was done

--- src/camouse/ScrollEventTest.java
+++ src/camouse/ScrollEventTest.java
@@ -3,10 +3,10 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 
 public class ScrollEventTest {
-    private static Robot robot;
+    private Robot robot;
     //private static Point p;
-    private static int initialX;
-    private static int initialY;
+    private int initialX;
+    private int initialY;
 
     public ScrollEventTest(int x, int y){
         try {
@@ -19,30 +19,30 @@ public class ScrollEventTest {
         this.initialY = y;
     }
 
-    public static void singleClick(){
+    public void singleClick(){
         this.robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         this.robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
     }
 
-    public static void doubleClick(){
+    public void doubleClick(){
         singleClick();
         singleClick();
     }
 
-    public static void rightClick(){
+    public void rightClick(){
         this.robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
         this.robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
     }
 
-    public static void scrollDown(){
+    public void scrollDown(){
         this.robot.mouseWheel(1);
     }
 
-    public static void scrollUp(){
+    public void scrollUp(){
         this.robot.mouseWheel(-1);
     }
 
-    public static void mouseMovement(int differenceX, int differenceY){
+    public void mouseMovement(int differenceX, int differenceY){
         //p = MouseInfo.getPointerInfo().getLocation();
         //robot.mouseMove(p.x+differenceX, p.y+differenceY);
         this.robot.mouseMove(initialX+differenceX, initialY+differenceY);
